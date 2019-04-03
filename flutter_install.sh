031afc053fb8caec508d51705e80a41d3e86fbf0
Arnaud GISSINGER <agissing@e3r3p18.42.fr>
2019-04-03 19:07:38 +0200

    progress bar + chmod 777

--- flutter_install.sh
+++ flutter_install.sh
@@ -1,17 +1,18 @@
 #!/bin/zsh
 
-if [[ $(ls /private/tmp/flutter/bin 2&> /dev/null ) ]]; then
+if [[ $(ls /tmp/flutter/bin 2&> /dev/null ) ]]; then
 	echo "Flutter already installed!";
 else
 	data=$(curl --silent "https://storage.googleapis.com/flutter_infra/releases/releases_macos.json" | grep -m 2 -A 4 '"channel": "stable"')
 	url=$(echo $data | grep archive | cut -d '"' -f4)
-	curl --silent "https://storage.googleapis.com/flutter_infra/releases/$url" > /private/tmp/flutter.zip;
-	unzip -q /private/tmp/flutter.zip -d /private/tmp/.;
-	rm -f /private/tmp/flutter.zip;
+	curl --progress-bar "https://storage.googleapis.com/flutter_infra/releases/$url" > /tmp/flutter.zip;
+	unzip -q /tmp/flutter.zip -d /tmp/.;
+	rm -f /tmp/flutter.zip;
 	if [[ -z $(echo $PATH | grep /tmp/flutter/bin) ]]; then
 		export PATH="$PATH:/tmp/flutter/bin";
 		source ~/.zshrc > /dev/null;
 	fi
+	chmod -R 777 "/tmp/flutter"
 	flutter doctor;
 	echo "Flutter installation done!";
 	zsh;
